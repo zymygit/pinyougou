fe4865afabf253ad5c13d3b6a701eaaa4b711528
zy <zy@itheima.com>
2019-04-19 01:03:39 +0800

    加密后登陆失败  未解决

--- pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -1,5 +1,42 @@
 package com.pinyougou.service;
 
-public class UserDetailsServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+		private SellerService service;
+		
+		public void setService(SellerService service) {
+			this.service = service;
+		}
+
+		@Override
+		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<GrantedAuthority> grantedAuths=new ArrayList<>();
+		grantedAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+		
+		
+		TbSeller seller = service.findOne(username);
+		System.out.println(seller.getStatus());
+		if(seller!=null) {
+			if(seller.getStatus().equals("1")) {
+				return new User(username, "123456", grantedAuths);
+			}else {
+				return null;
+			}
+		}else {
+			return null;
+		}
+	}
 
 }
