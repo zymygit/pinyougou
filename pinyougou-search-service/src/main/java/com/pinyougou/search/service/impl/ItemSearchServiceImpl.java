8ec573bf07f7a83fe5eb56738de7f427018009f3
zy <zy@itheima.com>
2019-04-29 16:02:23 +0800

    搜索分页

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -10,10 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.FilterQuery;
 import org.springframework.data.solr.core.query.GroupOptions;
 import org.springframework.data.solr.core.query.HighlightOptions;
 import org.springframework.data.solr.core.query.HighlightQuery;
 import org.springframework.data.solr.core.query.Query;
+import org.springframework.data.solr.core.query.SimpleFilterQuery;
 import org.springframework.data.solr.core.query.SimpleHighlightQuery;
 import org.springframework.data.solr.core.query.SimpleQuery;
 import org.springframework.data.solr.core.query.result.GroupEntry;
@@ -26,7 +28,7 @@ import org.springframework.data.solr.core.query.result.ScoredPage;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
-@Service(timeout=3000)
+@Service(timeout=2000)
 public class ItemSearchServiceImpl implements ItemSearchService {
 	@Autowired
 	private SolrTemplate solrTemplate;
@@ -34,25 +36,31 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 	private RedisTemplate redisTemplate;
 	@Override
 	public Map<String, Object> search(Map specMap) {
-
+			String str=(String)specMap.get("keywords");
+			specMap.put("keywords", str.replace(" ", ""));
 			/*HashMap map = new HashMap<>();
 			Query query=new SimpleQuery();
 			Criteria criteria=new Criteria("item_keywords").is(specMap.get("keywords"));
 			query.addCriteria(criteria);
 			ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
 			map.put("rows", page.getContent());*/
-			//1.按关键字查询（高亮显示）
+			//1.1按关键字查询（高亮显示）
 			Map<String,Object> map = new HashMap<>();
 			map.putAll(searchMap(specMap));
+			
 			//2.根据关键字查询商品分类
 			List<String> list = searchCategoryList(specMap);
 			map.put("categoryList", list);
 			//3.查询品牌和规格列表
-			if(list.size()>0) {
-				map.putAll(searchBrandAndSpecList(list.get(0)));
+			String name = (String)specMap.get("category");
+			if(!"".equals(name)) {//如果有分类名称
+				map.putAll(searchBrandAndSpecList(name));
+			}else {//如果没有分类名称，按照第一个查询
+				if(list.size()>0) {
+					map.putAll(searchBrandAndSpecList(list.get(0)));
+				}
 			}
-			
-			
+
 			return map;	
 	}
 	/**
@@ -70,6 +78,54 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 		//按照用户输入查找
 		Criteria criteria=new Criteria("item_keywords").is(specMap.get("keywords"));
 		query.addCriteria(criteria);
+		//1.2 按分类筛选
+		if(!"".equals(specMap.get("category"))) {
+			Criteria filterCriteria=new Criteria("item_category").is(specMap.get("category"));
+			FilterQuery filterQuery=new SimpleFilterQuery(filterCriteria);
+			query.addFilterQuery(filterQuery);
+		}
+		//1.3 按品牌筛选
+		if(!"".equals(specMap.get("brand"))) {
+			Criteria filterCriteria=new Criteria("item_brand").is(specMap.get("brand"));
+			FilterQuery filterQuery=new SimpleFilterQuery(filterCriteria);
+			query.addFilterQuery(filterQuery);
+		}
+		//1.4 过滤规格
+		if(specMap.get("spec")!=null) {
+			Map<String, String> map2 = (Map)specMap.get("spec");
+			for (String key : map2.keySet()) {
+				Criteria filterCriteria=new Criteria("item_spec_"+key).is(map2.get(key));
+				FilterQuery filterQuery=new SimpleFilterQuery(filterCriteria);
+				query.addFilterQuery(filterQuery);
+			}
+			
+		}
+		//1.5按价格筛选
+		if(!"".equals(specMap.get("price"))) {
+			String priceStr =(String)specMap.get("price");
+			String[] price = priceStr.split("-");
+			if(!price[0].equals("0")) {//如果区间起点不等于 0
+				Criteria filterCriteria=new Criteria("item_price").greaterThanEqual(price[0]);
+				FilterQuery filterquery=new SimpleFilterQuery(filterCriteria);
+				query.addFilterQuery(filterquery);
+			}
+			if(!price[1].equals("*")) {//如果区间终点不等于 *
+				Criteria filterCriteria=new Criteria("item_price").lessThanEqual(price[1]);
+				FilterQuery filterquery=new SimpleFilterQuery(filterCriteria);
+				query.addFilterQuery(filterquery);
+			}
+		}
+		//1.6 分页查询
+		Integer pageNo = (Integer) specMap.get("pageNo");
+		if(pageNo==null) {
+			pageNo=1;//默认第一页
+		}
+		Integer pageSize = (Integer) specMap.get("pageSize");
+		if(pageSize==null) {
+			pageSize=20;//默认显示20条
+		}
+		query.setOffset((pageNo-1)*pageSize);
+		query.setRows(pageSize);
 		HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
 		List<HighlightEntry<TbItem>> list = page.getHighlighted();
 		for (HighlightEntry<TbItem> h : list) {//循环高亮入口集合
@@ -80,6 +136,8 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 			}
 		}
 		map.put("rows", page.getContent());
+		map.put("totalPages", page.getTotalPages());
+		map.put("totalCount", page.getTotalElements());
 		return map;
 	}
 	/**
--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,10 +1,90 @@
 app.controller("searchController",function($scope,searchService){
+	//搜索
 	$scope.search=function(){
+		if($scope.searchMap.keywords==""){
+			return;
+		}
+		$scope.searchMap.pageNo= parseInt($scope.searchMap.pageNo) ;
 		searchService.search($scope.searchMap).success(
 				function(response){
 					$scope.resultMap=response;
+					buildPageLabel();
 				}
 		);
+		
+	}
+	//添加搜索项
+	$scope.searchMap={"keywords":"","category":"","brand":"","spec":{},"price":"","pageNo":1,"pageSize":40};
+	$scope.addSearchItem=function(key,value){
+		if(key=="category"||key=="brand"||key=="price"){//如果点击的是分类或者是品牌
+			$scope.searchMap[key]=value;
+		}else{//否则是规格
+			$scope.searchMap.spec[key]=value;
+		}
+		$scope.search();//执行搜索
+	}
+	//移除复合搜索条件
+	$scope.removeSearchItem=function(key){
+		if(key=="category"||key=="brand"||key=="price"){//如果是分类或品牌
+			$scope.searchMap[key]="";
+		}else{//否则是规格
+			delete $scope.searchMap.spec[key];//移除此属性
+		}
+		$scope.search();//执行搜索
+	}
+	//页码逻辑
+	buildPageLabel=function(){
+		$scope.pageLabel=[];
+		var firstPage=1;//开始页码
+		var lastPage=$scope.resultMap.totalPages;//截止页码
+		$scope.firstDot=true;//前面有点
+		$scope.lastDot=true;//后边有点
+		if($scope.resultMap.totalPages>5){//如果总页数大于 5 页,显示部分页码
+			
+			if($scope.searchMap.pageNo<=3){//如果当前页小于等于 3
+				lastPage=5;
+				$scope.firstDot=false;
+			}else if($scope.searchMap.pageNo>=$scope.resultMap.totalPages-2){//如果当前页大于等于最大页码-2
+				firstPage=$scope.resultMap.totalPages-4;
+				$scope.lastDot=false;
+			}else{ //显示当前页为中心的 5 页
+				firstPage=$scope.searchMap.pageNo-2;
+				lastPage=$scope.searchMap.pageNo+2;
+			}
+			
+		}else{
+			$scope.firstDot=false;
+			$scope.lastDot=false;
+		}
+		//循环产生页码标签
+		for(var i=firstPage;i<=lastPage;i++){
+			$scope.pageLabel.push(i);
+		}
+	}
+	//根据用户点击或者输入的页码进行跳转
+	$scope.queryByPage=function(pageNo){
+		
+		if(pageNo<1 || pageNo>$scope.resultMap.totalPages){
+			return;
+			}
+		$scope.searchMap.pageNo=pageNo;
+		$scope.search();//执行搜索
 	}
 	
+	//判断当前页是否为第一页
+	$scope.first=function(){
+		if($scope.searchMap.pageNo==1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//判断当前页是否为最后页
+	$scope.last=function(){
+		if($scope.resultMap.totalPages==$scope.searchMap.pageNo){
+			return true;
+		}else{
+			return false;
+		}
+	}
 });
